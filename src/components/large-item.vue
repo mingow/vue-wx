f1bd77eb6726a4e28c3734ee86bc3621f25f8513
wangmeng <wangmeng@plfg.net>
2019-10-11 15:59:09 +0800

    first init

---
+++ src/components/large-item.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <image class="card" v-bind:style="computedClassObject" v-bind:src="src"></image>
+    <p v-bind:style="style">demo</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    color: {
+      type: String,
+      default: 'blue'
+    },
+    imageWidth: {
+      type: String,
+      default: '100%'
+    },
+    imageHeight: {
+      type: String,
+      default: '100%'
+    }
+  },
+  data () {
+    return {
+      imageStyle: {
+        height: '100%'
+      }
+    }
+  },
+  methods: {
+    showJson  (style) {
+      var s = []
+      for (let i in style) {
+        s.push(i + ':' + style[i])
+      }
+      s = s.join(';')
+      return s
+    }
+  },
+  computed: {
+    computedClassObject () {
+      return this.showJson({
+        height: Number(this.imageHeight) ? this.imageHeight + 'px' : this.imageHeight,
+        width: Number(this.imageWidth) ? this.imageWidth + 'px' : this.imageWidth
+      })
+    }
+  }
+}
+</script>
+
+<style>
+.card {
+  padding: 10px;
+}
+</style>
